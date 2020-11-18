86fd570ec8c08a22d4e3b6f0c7687f55cdcedb9d
Danverr <79198736000@mail.ru>
2020-11-18 22:19:15 +0300

    TypeList Solution

--- module-1/homework/TypeList/typelist/erase.h
+++ module-1/homework/TypeList/typelist/erase.h
@@ -4,3 +4,19 @@
 
 template<typename TList, typename TargetType>
 struct Erase;
+
+template<typename Head, typename Tail, typename TargetType>
+struct Erase<TypeList<Head, Tail>, TargetType>{
+    using NewTypeList = TypeList<Head, typename Erase<Tail, TargetType>::NewTypeList>;
+};
+
+template<typename Tail, typename TargetType>
+struct Erase<TypeList<TargetType, Tail>, TargetType>{
+    using NewTypeList = Tail;
+};
+
+
+template<typename TargetType>
+struct Erase<NullType, TargetType>{
+    using NewTypeList = NullType;
+};
--- module-1/homework/TypeList/typelist/replace.h
+++ module-1/homework/TypeList/typelist/replace.h
@@ -4,3 +4,18 @@
 
 template<typename TList, typename OldType, typename NewType> 
 struct Replace;
+
+template<typename Head, typename Tail, typename OldType, typename NewType>
+struct Replace<TypeList<Head, Tail>, OldType, NewType>{
+    using NewTypeList = TypeList<Head, typename Replace<Tail, OldType, NewType>::NewTypeList>;
+};
+
+template<typename Tail, typename OldType, typename NewType>
+struct Replace<TypeList<OldType, Tail>, OldType, NewType>{
+    using NewTypeList = TypeList<NewType, Tail>;
+};
+
+template<typename OldType, typename NewType>
+struct Replace<NullType, OldType, NewType>{
+    using NewTypeList = NullType;
+};
